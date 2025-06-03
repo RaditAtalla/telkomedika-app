d041e53656d9eba80e3803f33c2f60dc0f3b1d1f
ShykiFr <moh.dzaki.ayatillah@gmail.com>
2025-06-03 17:35:23 +0700

    Menambah isi kelas CodeGeneration

--- src/main/java/com/dpbo/telkomedika/CodeGeneration.java
+++ src/main/java/com/dpbo/telkomedika/CodeGeneration.java
@@ -1,9 +1,24 @@
 package com.dpbo.telkomedika;
 
+import java.util.Random;
+
 public class CodeGeneration {
 	private String code;
+	private static final String character = "ABCDEFGHIJKLMOPQRSTUVWXYZ1234567890";
+	private static Random random = new Random();
 	
 	public void generateCode() {
-		
+		String hasil = "";
+		for (int i = 0; i < 6; i++) {
+			int index = random.nextInt(character.length());
+			hasil += character.charAt(index);
+		}
+		this.code = hasil;
 	}
+
+	public String getCode() {
+		return code;
+	}
+	
+	
 }
